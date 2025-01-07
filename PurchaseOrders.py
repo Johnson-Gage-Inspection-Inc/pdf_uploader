69e6f53754c62d2f06a16dfc3a59102dc1ff2454
Jeff Hall <jeffh@jgiquality.com>
2025-01-07 17:51:54 -0600

    init

--- PurchaseOrders.py
+++ PurchaseOrders.py
@@ -6,9 +6,10 @@ import os
 import app.api as api
 from config import *
 
-dt_format = "%Y-%m-%dT%H:%M:%S"
+DT_FORMAT = "%Y-%m-%dT%H:%M:%S"
 
-def update_dict(dict, response):
+
+def update_dict(dict: dict, response: list) -> dict:
     for so in response:
         PrimaryPo = so['PoNumber']
         SecondaryPo = so['SecondaryPo']
@@ -23,20 +24,32 @@ def update_dict(dict, response):
             dict[SecondaryPo].append(ServiceOrderId)
     return dict
 
-def _get_PO_numbers(token, start_str="2020-08-13T00:00:00", end_str=dt.datetime.now().strftime(dt_format), increment=91):
+
+def _get_PO_numbers(token: str, start_str="2020-08-13T00:00:00", end_str=dt.datetime.now().strftime(DT_FORMAT), increment=91):
+    """ Get a dictionary of PO numbers and their corresponding service order IDs from the API.
+
+    Args:
+        token (str): The API token.
+        start_str (str, optional): Start date for PO search. Format: "%Y-%m-%dT%H:%M:%S". Defaults to "2020-08-13T00:00:00".
+        end_str (str, optional): End date for PO search. Format: "%Y-%m-%dT%H:%M:%S". Defaults to dt.datetime.now().strftime(DT_FORMAT).
+        increment (int, optional): Number of days to search at a time. Defaults to 91.
+
+    Returns:
+        dict: A dictionary of PO numbers and their corresponding service order IDs.
+    """
     dict = {}
 
-    start_date = dt.datetime.strptime(start_str, dt_format)
-    end_date = dt.datetime.strptime(end_str, dt_format)
+    start_date = dt.datetime.strptime(start_str, DT_FORMAT)
+    end_date = dt.datetime.strptime(end_str, DT_FORMAT)
 
     from_date = start_date
     to_date = from_date + dt.timedelta(days=increment)
 
     while True:
-        print("Getting service orders from " + from_date.strftime(dt_format) + " to " + to_date.strftime(dt_format) + "...")
+        print(f"Getting service orders from {from_date.strftime(DT_FORMAT)} to {to_date.strftime(DT_FORMAT)}...")
         data = {
-            'from': from_date.strftime(dt_format),
-            'to': to_date.strftime(dt_format)
+            'from': from_date.strftime(DT_FORMAT),
+            'to': to_date.strftime(DT_FORMAT)
         }  # Set the parameters for the API call
         response = api.get_service_orders(data, token)
         dict = update_dict(dict, response)
@@ -47,17 +60,34 @@ def _get_PO_numbers(token, start_str="2020-08-13T00:00:00", end_str=dt.datetime.
     print("Done.")
     return dict
 
-def update_PO_numbers(file_path, token, modified_after = None):
+
+def update_PO_numbers(file_path: str, token: str, modified_after: str = None) -> dict:
+    """ Update the PO dictionary with new PO numbers from the API.
+
+    Args:
+        file_path (str): File path for the PO dictionary.
+        token (str): The API token.
+        modified_after (str, optional): Only get service orders modified after this date. Format: "%Y-%m-%dT%H:%M:%S". Defaults to None.
+
+    Returns:
+        dict: A dictionary of PO numbers and their corresponding service order IDs.
+    """
     # Read the compressed dictionary from the file
-    dict = expand_from_file(file_path)
-        
+    try:
+        dict = expand_from_file(file_path)
+    except gzip.BadGzipFile:
+        dict = _get_PO_numbers(token)
+        save_as_zip_file(file_path, dict)
+    except:
+        raise
+
     current_datetime = dt.datetime.now()                                    # Get the current datetime
 
     timestamp = os.path.getmtime(file_path)                                 # Get the timestamp of the last modification
     last_modified = dt.datetime.fromtimestamp(timestamp)                    # Convert the timestamp to a datetime object
 
     if modified_after is None:
-        modified_after = last_modified.strftime(dt_format)
+        modified_after = last_modified.strftime(DT_FORMAT)
 
     # Check if the file has been modified since the last update
     if last_modified < current_datetime:
@@ -71,18 +101,43 @@ def update_PO_numbers(file_path, token, modified_after = None):
     print("No changes detected since the last update.")
     return dict
 
-def save_as_zip_file(file_path, dict):
+
+def save_as_zip_file(file_path: str, dict: dict):
+    """ Compress the dictionary and write to the file.
+
+    Args:
+        file_path (str): File path for the PO dictionary.
+        dict (dict): A dictionary of PO numbers and their corresponding service order IDs.
+    """
     with gzip.open(file_path, 'wb') as file:
         json_data = json.dumps(dict).encode('utf-8')
         file.write(json_data)
 
-def expand_from_file(file_path):
+
+def expand_from_file(file_path: str):
+    """ Read the compressed dictionary from the file.
+
+    Args:
+        file_path (str): File path for the PO dictionary.
+
+    Returns:
+        dict: A dictionary of PO numbers and their corresponding service order IDs.
+    """
     with gzip.open(file_path, 'rb') as file:
         json_data = file.read()
         dict = json.loads(json_data.decode('utf-8'))
     return dict
 
-def extract_po(filename):
+
+def extract_po(filename: str) -> str:
+    """ Extract the PO number from the filename.
+
+    Args:
+        filename (str): The filename.
+
+    Returns:
+        str: The PO number.
+    """
     possible_delimiters = [" ", "_", "-", "#"]
     po = filename.replace(' - ', '-')
     for delimiter in possible_delimiters:
@@ -96,8 +151,7 @@ def extract_po(filename):
                 po = possible_po
     return po
 
-# Set the file path for the PO dictionary
 if os.path.exists('app'):
-    file_path = 'app/dict.json.gz' # Local path
+    file_path = 'app/dict.json.gz'  # Local path
 else:
-    file_path = 'C:\\uploader\\app\\dict.json.gz' # Server path
+    file_path = 'C:\\uploader\\app\\dict.json.gz'  # Server path
