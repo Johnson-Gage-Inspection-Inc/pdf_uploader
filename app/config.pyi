06cb1435040cd30d2f88eb5c33d1c94849995203
Jeff Hall <rhythmatician5@gmail.com>
2026-03-01 18:21:30 -0600

    Add type stubs for app.config to expose config_manager values

---
+++ app/config.pyi
@@ -0,0 +1,11 @@
+"""Type stubs for app.config -- exposes config_manager values as module attributes."""
+
+MAX_RUNTIME: int | None
+DEBUG: bool
+DELETE_MODE: bool
+tesseract_cmd_path: str
+user_folder: str
+SHAREPOINT_PATH: str
+LOG_FILE: str
+PO_DICT_FILE: str
+QUALER_ENDPOINT: str
